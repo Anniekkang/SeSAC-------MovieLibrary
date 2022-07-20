de4ec34c2aed0731c617e9cf4cfaec1510634412
annie <knr9144@icloud.com>
2022-07-20 21:35:43 +0900

    title, overview 구현

--- LibraryProject/libraryCollectionViewController.swift
+++ LibraryProject/libraryCollectionViewController.swift
@@ -16,9 +16,6 @@ class libraryCollectionViewController: UICollectionViewController {
         super.viewDidLoad()
 
         //컬렉션뷰의 셀 크기, 셀 사이의 간격 등등 설정 (Estimate Size None으로 설정)
-        
-        
-        
         let layout = UICollectionViewFlowLayout()
         let spacing :CGFloat = 6
         let width = UIScreen.main.bounds.width - (3 * spacing)
@@ -29,6 +26,8 @@ class libraryCollectionViewController: UICollectionViewController {
         layout.sectionInset = UIEdgeInsets(top: 6, left: 6, bottom: 6, right: 6)
         collectionView.collectionViewLayout = layout
         
+        
+        //컬렉션에 구조체의 정보 넣기
         
         
     }
@@ -43,6 +42,11 @@ class libraryCollectionViewController: UICollectionViewController {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "libraryCollectionViewCell", for: indexPath) as! libraryCollectionViewCell
         
+        let data = movieLibrary.movie[indexPath.row]
+        
+        
+        cell.movieoverviewLabel.text = data.overview
+        cell.movietitleLabel.text = data.title
         cell.contentView.layer.cornerRadius = 20
         cell.backgroundImageView.backgroundColor = color.randomElement()
         cell.movietitleLabel.textColor = .white
